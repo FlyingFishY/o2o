6071277de0494e2e7136bf1a5f1810888c6e1944
yanghao <cv78619233@qq.com>
2019-06-05 22:01:37 +0800

    8-6商品添加之前端实现

--- src/main/java/com/imooc/o2o/web/shopadmin/ShopAdminController.java
+++ src/main/java/com/imooc/o2o/web/shopadmin/ShopAdminController.java
@@ -9,18 +9,27 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class ShopAdminController {
 	@RequestMapping(value="/shopoperation")
 	public String shopOperation() {
+		//转发至店铺操作页面
 		return "shop/shopoperation";
 	}
 	@RequestMapping(value="/shoplist")
 	public String shopList() {
+		//转发至店铺列表页面
 		return "shop/shoplist";
 	}
 	@RequestMapping(value="/shopmanagement")
 	public String shopManagement() {
+		//转发至店铺管理页面
 		return "shop/shopmanagement";
 	}
 	@RequestMapping(value="/productcategorymanagement")
 	public String productCategoryManage() {
+		//转发至商品类别管理页面
 		return "shop/productcategorymanagement";
+	}
+	@RequestMapping(value="/productoperation")
+	public String productOperation() {
+		//转发至商品添加/编辑页面
+		return "shop/productoperation";
 	}
 }
